a128b7cee406fea1478ee7783a0e3a86bdedb19a
Mike Gehard <mike.gehard@gmail.com>
2014-05-11 17:56:33 -0600

    Fix groupId

--- src/main/java/io/github/msgehard/taskManager/web/App.java
+++ src/main/java/io/github/msgehard/taskManager/web/App.java
@@ -1,11 +1,12 @@
+package io.github.msgehard.taskManager.web;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-public class Example {
+public class App {
 
     @RequestMapping("/")
     String home() {
@@ -13,7 +14,7 @@ public class Example {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
---
+++ src/main/java/io/github/msgehard/taskManager/web/HomeController.java
@@ -0,0 +1,7 @@
+package io.github.msgehard.taskManager.web;
+
+/**
+ * Created by mike on 5/11/14.
+ */
+public class HomeController {
+}
