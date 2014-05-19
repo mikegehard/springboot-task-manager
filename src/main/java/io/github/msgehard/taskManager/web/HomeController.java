5fb7b765b4bce5f385b8a1c6c56ba9e6e00b6784
Mike Gehard <mike.gehard@gmail.com>
2014-05-19 12:53:05 -0600

    Convert over to using Thymeleaf templates

--- src/main/java/io/github/msgehard/taskManager/web/HomeController.java
+++ src/main/java/io/github/msgehard/taskManager/web/HomeController.java
@@ -1,19 +1,22 @@
 package io.github.msgehard.taskManager.web;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
-    String home() {
-        return "Hello World!";
+    String home(Model model) {
+        model.addAttribute("welcome", "Hello world!");
+        return "home/index";
     }
 
     @RequestMapping(value= "/{name}", method = RequestMethod.GET)
-    public String homeWithName(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public String homeWithName(@PathVariable String name, Model model) {
+        model.addAttribute("welcome", "Hello " + name + "!");
+        return "home/index";
     }
 }
