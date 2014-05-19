475e431d2ca68cfe1f7bb4a8aeba939e95747202
Mike Gehard <mike.gehard@gmail.com>
2014-05-18 19:45:11 -0600

    Allow passing in URI parameters

--- src/main/java/io/github/msgehard/taskManager/web/HomeController.java
+++ src/main/java/io/github/msgehard/taskManager/web/HomeController.java
@@ -1,11 +1,19 @@
 package io.github.msgehard.taskManager.web;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping(value= "/{name}", method = RequestMethod.GET)
+    public String homeWithName(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
