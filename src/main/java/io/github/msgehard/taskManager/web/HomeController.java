060a7fbf7cb80e0a8a1b694e12292013eb62d373
Mike Gehard <mike.gehard@gmail.com>
2014-05-19 20:57:47 -0600

    Get index page working
    This uses a header and a footer fragment

--- src/main/java/io/github/msgehard/taskManager/web/HomeController.java
+++ src/main/java/io/github/msgehard/taskManager/web/HomeController.java
@@ -1,22 +1,15 @@
 package io.github.msgehard.taskManager.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
     String home(Model model) {
-        model.addAttribute("welcome", "Hello world!");
-        return "home/index";
-    }
-
-    @RequestMapping(value= "/{name}", method = RequestMethod.GET)
-    public String homeWithName(@PathVariable String name, Model model) {
-        model.addAttribute("welcome", "Hello " + name + "!");
+        model.addAttribute("welcome", "Welcome to the task manager!");
         return "home/index";
     }
 }
