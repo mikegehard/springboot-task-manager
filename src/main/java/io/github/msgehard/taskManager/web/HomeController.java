ed51db45a6a02c9498f60f7cb417e75076583023
Mike Gehard <mike.gehard@gmail.com>
2014-08-07 17:30:47 -0600

    Upgrade spring boot
    Can't use 1.1.5 because for some reason it can't find the
    @RequestMapping annotation.

--- src/main/java/io/github/msgehard/taskManager/web/HomeController.java
+++ src/main/java/io/github/msgehard/taskManager/web/HomeController.java
@@ -4,18 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView view = new ModelAndView("home/index");
-        view.addObject("message", "Welcome to the task manager!");
-
-        return view;
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the task manager!");
+        return "home/index";
     }
 }
